c8c65b96905d8f296229dc3e833d23aa84b3c8fa
dponna534 <Deepak_Ponnath2@comcast.com>
2021-02-26 08:58:35 +0530

    RDKCOM-1920 : XCast support for Firebolt UI
    Reason for change: Added xcast support in the firebolt ui for Youtube and Amazon.
    Test Procedure: None.
    Risks: Low
    
    Signed-off-by: Deepak_Ponnath2@comcast.com
    Change-Id: I9c78d740f91f8ee5d5125e00840ab03668f2a77b

--- accelerator-home-ui/src/App.js
+++ accelerator-home-ui/src/App.js
@@ -16,30 +16,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-import { Utils, Router, Storage } from '@lightningjs/sdk'
+import { Utils, Router, Storage } from '@lightningjs/sdk';
 import ThunderJS from 'ThunderJS';
-import routes from './api/routes'
+import routes from './api/routes';
 import AppApi from '../src/api/AppApi.js';
+import XcastApi from '../src/api/XcastApi';
+
+const config = {
+  host: '127.0.0.1',
+  port: 9998,
+  default: 1,
+};
+var thunder = ThunderJS(config);
+var appApi = new AppApi();
 
 export default class App extends Router.App {
   static getFonts() {
-    return [{ family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') }]
+    return [{ family: 'Regular', url: Utils.asset('fonts/Roboto-Regular.ttf') }];
   }
   _setup() {
-    Router.startRouter(routes, this)
-    document.onkeydown = (e) => {
+    Router.startRouter(routes, this);
+    document.onkeydown = e => {
       if (e.keyCode == 8) {
-        e.preventDefault()
+        e.preventDefault();
       }
     };
   }
 
   _init() {
-    const config = {
-      host: '127.0.0.1',
-      port: 9998,
-      default: 1,
-    };
+    this.xcastApi = new XcastApi();
     var thunder = ThunderJS(config);
     const rdkshellCallsign = 'org.rdk.RDKShell';
     thunder.Controller.activate({ callsign: rdkshellCallsign })
@@ -173,7 +178,7 @@ export default class App extends Router.App {
       })
       .catch(err => {
         console.log('Error', err);
-      })
+      });
   }
 
   deactivateChildApp(plugin) {
@@ -198,16 +203,132 @@ export default class App extends Router.App {
     }
   }
 
+  /**
+   * Function to register event listeners for Xcast plugin.
+   */
+  registerXcastListeners() {
+    this.xcastApi.registerEvent('onApplicationLaunchRequest', notification => {
+      console.log('Received a launch request ' + JSON.stringify(notification));
+      if (this.xcastApps(notification.applicationName)) {
+        let applicationName = this.xcastApps(notification.applicationName);
+        console.log('Launch ' + this.xcastApps(notification.applicationName));
+        if (applicationName == 'Amazon' && Storage.get('applicationType') != 'Amazon') {
+          this.deactivateChildApp(Storage.get('applicationType'));
+          appApi.launchAmazon();
+          Storage.set('applicationType', 'Amazon');
+          appApi.setVisibility('ResidentApp', false);
+          let params = { applicationName: notification.applicationName, state: 'running' };
+          this.xcastApi.onApplicationStateChanged(params);
+        } else if (applicationName == 'Cobalt' && Storage.get('applicationType') != 'Cobalt') {
+          this.deactivateChildApp(Storage.get('applicationType'));
+          appApi.launchCobalt();
+          Storage.set('applicationType', 'Cobalt');
+          appApi.setVisibility('ResidentApp', false);
+          let params = {
+            applicationName: notification.applicationName,
+            state: 'running',
+          };
+          this.xcastApi.onApplicationStateChanged(params);
+        }
+      }
+    });
+    this.xcastApi.registerEvent('onApplicationHideRequest', notification => {
+      console.log('Received a hide request ' + JSON.stringify(notification));
+      if (this.xcastApps(notification.applicationName)) {
+        let applicationName = this.xcastApps(notification.applicationName);
+        console.log('Hide ' + this.xcastApps(notification.applicationName));
+        if (applicationName === 'Amazon' && Storage.get('applicationType') === 'Amazon') {
+          appApi.suspendAmazon();
+          let params = { applicationName: notification.applicationName, state: 'stopped' };
+          this.xcastApi.onApplicationStateChanged(params);
+        } else if (applicationName === 'Cobalt' && Storage.get('applicationType') === 'Cobalt') {
+          appApi.suspendCobalt();
+          let params = { applicationName: notification.applicationName, state: 'stopped' };
+          this.xcastApi.onApplicationStateChanged(params);
+        }
+        Storage.set('applicationType', '');
+        appApi.setVisibility('ResidentApp', true);
+        thunder.call('org.rdk.RDKShell', 'moveToFront', { client: 'ResidentApp' }).then(result => {
+          console.log('ResidentApp moveToFront Success');
+        });
+      }
+    });
+    this.xcastApi.registerEvent('onApplicationResumeRequest', notification => {
+      console.log('Received a resume request ' + JSON.stringify(notification));
+      if (this.xcastApps(notification.applicationName)) {
+        let applicationName = this.xcastApps(notification.applicationName);
+        console.log('Resume ' + this.xcastApps(notification.applicationName));
+        if (applicationName == 'Amazon' && Storage.get('applicationType') != 'Amazon') {
+          this.deactivateChildApp(Storage.get('applicationType'));
+          appApi.launchAmazon();
+          Storage.set('applicationType', 'Amazon');
+          appApi.setVisibility('ResidentApp', false);
+          let params = { applicationName: notification.applicationName, state: 'running' };
+          this.xcastApi.onApplicationStateChanged(params);
+        } else if (applicationName == 'Cobalt' && Storage.get('applicationType') != 'Cobalt') {
+          this.deactivateChildApp(Storage.get('applicationType'));
+          appApi.launchCobalt();
+          Storage.set('applicationType', 'Cobalt');
+          appApi.setVisibility('ResidentApp', false);
+          let params = { applicationName: notification.applicationName, state: 'running' };
+          this.xcastApi.onApplicationStateChanged(params);
+        }
+      }
+    });
+    this.xcastApi.registerEvent('onApplicationStopRequest', notification => {
+      console.log('Received a stop request ' + JSON.stringify(notification));
+      if (this.xcastApps(notification.applicationName)) {
+        console.log('Stop ' + this.xcastApps(notification.applicationName));
+        let applicationName = this.xcastApps(notification.applicationName);
+        if (applicationName === 'Amazon' && Storage.get('applicationType') === 'Amazon') {
+          appApi.deactivateAmazon();
+          Storage.set('applicationType', '');
+          appApi.setVisibility('ResidentApp', true);
+          thunder.call('org.rdk.RDKShell', 'moveToFront', { client: 'ResidentApp' }).then(result => {
+            console.log('ResidentApp moveToFront Success');
+          });
+          let params = { applicationName: notification.applicationName, state: 'stopped' };
+          this.xcastApi.onApplicationStateChanged(params);
+        } else if (applicationName === 'Cobalt' && Storage.get('applicationType') === 'Cobalt') {
+          appApi.deactivateCobalt();
+          Storage.set('applicationType', '');
+          appApi.setVisibility('ResidentApp', true);
+          thunder.call('org.rdk.RDKShell', 'moveToFront', { client: 'ResidentApp' }).then(result => {
+            console.log('ResidentApp moveToFront Success');
+          });
+          let params = { applicationName: notification.applicationName, state: 'stopped' };
+          this.xcastApi.onApplicationStateChanged(params);
+        }
+      }
+    });
+    this.xcastApi.registerEvent('onApplicationStateRequest', notification => {
+      console.log('Received a state request ' + JSON.stringify(notification));
+      if (this.xcastApps(notification.applicationName)) {
+        let applicationName = this.xcastApps(notification.applicationName);
+        let status = AppApi.pluginStatus(applicationName);
+        let params = { applicationName: notification.applicationName, state: 'stopped' };
+        if (status) {
+          params.status = 'running';
+        }
+        this.xcastApi.onApplicationStateChanged(params);
+        console.log('State of ' + this.xcastApps(notification.applicationName));
+      }
+    });
+  }
+
+  /**
+   * Function to get the plugin name for the application name.
+   * @param {string} app App instance.
+   */
+  xcastApps(app) {
+    if (Object.keys(XcastApi.supportedApps()).includes(app)) {
+      return XcastApi.supportedApps()[app];
+    } else return false;
+  }
+
   _handleKey(key) {
-    const config = {
-      host: '127.0.0.1',
-      port: 9998,
-      default: 1,
-    };
-    var thunder = ThunderJS(config);
-    var appApi = new AppApi();
     if (key.keyCode == 227) {
-      var showMax = 'https://ott-app.dstv.com/'
+      var showMax = 'https://ott-app.dstv.com/';
       this.deactivateChildApp(Storage.get('applicationType'));
       appApi.launchWeb(showMax);
       Storage.set('applicationType', 'WebApp');
---
+++ accelerator-home-ui/src/api/XcastApi.js
@@ -0,0 +1,128 @@
+/**
+ * If not stated otherwise in this file or this component's LICENSE
+ * file the following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+import ThunderJS from 'ThunderJS';
+/**
+ * Class for Xcast thunder plugin apis.
+ */
+
+export default class XcastApi {
+  constructor() {
+    console.log('Xcast constructor');
+    this._events = new Map();
+  }
+
+  /**
+   * Function to activate the Xcast plugin
+   */
+  activate() {
+    return new Promise((resolve, reject) => {
+      const config = {
+        host: '127.0.0.1',
+        port: 9998,
+        default: 1,
+      };
+      this._thunder = ThunderJS(config);
+      this.callsign = 'org.rdk.Xcast';
+      this._thunder
+        .call('Controller', 'activate', { callsign: this.callsign })
+        .then(result => {
+          console.log(result);
+          console.log('Xcast activation success');
+          this._thunder
+            .call('org.rdk.Xcast', 'getEnabled')
+            .then(result => {
+              if (result.success) {
+                console.log('Xcast enabled');
+                this._thunder.on(this.callsign, 'onApplicationLaunchRequest', notification => {
+                  console.log('onApplicationLaunchRequest ' + JSON.stringify(notification));
+                  if (this._events.has('onApplicationLaunchRequest')) {
+                    this._events.get('onApplicationLaunchRequest')(notification);
+                  }
+                });
+                this._thunder.on(this.callsign, 'onApplicationHideRequest', notification => {
+                  console.log('onApplicationHideRequest ' + JSON.stringify(notification));
+                  if (this._events.has('onApplicationHideRequest')) {
+                    this._events.get('onApplicationHideRequest')(notification);
+                  }
+                });
+                this._thunder.on(this.callsign, 'onApplicationResumeRequest', notification => {
+                  console.log('onApplicationResumeRequest ' + JSON.stringify(notification));
+                  if (this._events.has('onApplicationResumeRequest')) {
+                    this._events.get('onApplicationResumeRequest')(notification);
+                  }
+                });
+                this._thunder.on(this.callsign, 'onApplicationStopRequest', notification => {
+                  console.log('onApplicationStopRequest ' + JSON.stringify(notification));
+                  if (this._events.has('onApplicationStopRequest')) {
+                    this._events.get('onApplicationStopRequest')(notification);
+                  }
+                });
+                resolve(true);
+              } else {
+                console.log('Xcast enabled failed');
+              }
+            })
+            .catch(err => {
+              console.error('Enabling failure', err);
+              reject('Xcast enabling failed', err);
+            });
+        })
+        .catch(err => {
+          console.error('Activation failure', err);
+          reject('Xcast activation failed', err);
+        });
+    });
+  }
+
+  /**
+   *
+   * @param {string} eventId
+   * @param {function} callback
+   * Function to register the events for the Xcast plugin.
+   */
+  registerEvent(eventId, callback) {
+    this._events.set(eventId, callback);
+  }
+
+  /**
+   * Function to deactivate the Xcast plugin.
+   */
+  deactivate() {
+    this._events = new Map();
+    this._thunder = null;
+  }
+
+  /**
+   * Function to notify the state of the app.
+   */
+  onApplicationStateChanged(params) {
+    return new Promise((resolve, reject) => {
+      console.log('Notifying back');
+      this._thunder.call('org.rdk.Xcast', 'onApplicationStateChanged', params).then(result => {
+        console.log(result);
+        resolve(result);
+      });
+    });
+  }
+
+  static supportedApps() {
+    var xcastApps = { AmazonInstantVideo: 'Amazon', YouTube: 'Cobalt' };
+    return xcastApps;
+  }
+}
